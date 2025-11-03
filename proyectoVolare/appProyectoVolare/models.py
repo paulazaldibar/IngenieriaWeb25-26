5dc9abd790256fcbad8f4c41d292b50fa2ea133e
paulazaldibar <paulade.velasco@opendeusto.es>
2025-11-03 13:59:30 +0100

    Creacion de vistas y modelo(clases)

--- proyectoVolare/appProyectoVolare/models.py
+++ proyectoVolare/appProyectoVolare/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.db import models
 
-# Create your models here.
+class Aerolinea(models.Model):
+    id_aerolinea = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    siglas = models.CharField(max_length=10)
+    logo = models.ImageField(upload_to='logos_aerolineas/')
+    descripcion = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Pais(models.Model):
+    id_pais = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    bandera = models.ImageField(upload_to='banderas_paises/')
+    curiosidad = models.TextField()
+    
+    def __str__(self):
+        return self.nombre
+
+class Aeropuerto(models.Model):
+    id_aeropuerto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    pais = models.ForeignKey(Pais, on_delete=models.CASCADE)
+    foto = models.ImageField(upload_to='fotos_aeropuertos/')
+    aerolineas = models.ManyToManyField(Aerolinea)
+    
+    def __str__(self):
+        return self.nombre     	
