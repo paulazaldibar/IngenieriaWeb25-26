5b124e456a7c317f29aff2173aa2381cf2f8d1a4
jonru21 <jon.r@opendeusto.es>
2025-12-09 05:17:08 +0100

    cambio
    cambio

--- E4a/proyectoVolare/proyectoVolare/settings.py
+++ E4a/proyectoVolare/proyectoVolare/settings.py
@@ -82,6 +82,7 @@ TEMPLATES = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://provolare.netlify.app",
 ]
 
 WSGI_APPLICATION = 'proyectoVolare.wsgi.application'
